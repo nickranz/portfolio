b21b3313e2e31c8c510ab10b91b4957f696359ea
Nicolas Ranz <nranz1410@hotmail.com>
2024-05-16 14:12:53 -0400

    installed bootstrapped, imported into app.js to test it working. Plan on working on setting up HomePage tomorrow

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+// import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 function App() {
   return (
     <div className="App">
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        </Container>
+      </Navbar>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
